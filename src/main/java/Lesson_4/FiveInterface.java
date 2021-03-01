ae1a2d89f53fb982ad7adbc586f6f866b1689477
Andrew <andreykonovalov33rus@gmail.com>
2021-03-01 20:43:35 +0300

    Lesson_4_done!!!

---
+++ src/main/java/Lesson_4/FiveInterface.java
@@ -0,0 +1,8 @@
+package Lesson_4;
+
+import java.util.List;
+
+public interface FiveInterface {
+    List<String> aWordsAndThreeLetters (List<String> stringListList, List<String> voidList);
+
+}
---
+++ src/main/java/Lesson_4/FourInterface.java
@@ -0,0 +1,9 @@
+package Lesson_4;
+
+import java.util.List;
+
+public interface FourInterface {
+    double average (List<Integer> list);
+
+
+}
